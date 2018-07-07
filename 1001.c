483a45e104c19175c94adb2f49422d06a8c4d7c2
yanyuw <547274419@qq.com>
2018-07-07 23:24:10 +0800

    PAT 1001

---
+++ 1001.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+//测试点8
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int c = a + b;
+    if(c < 0){
+        printf("-");
+        c = -c;
+    }
+    char s[20];
+    sprintf(s, "%d", c);
+    int len = strlen(s);
+    int t = len%3;
+    for(int i = 0; i < t; i++){
+        printf("%c", s[i]);
+        if(i == t-1 && len != 1){
+            printf(",");
+        }
+    }
+    
+    for(int i = t; i < len; i++){
+        printf("%c", s[i]);
+        if((i-t+1) % 3 == 0 && i != len-1 ){
+            printf(",");
+        }
+    }
+    
+    
+    return 0;
+}
